ab7ffc3d7a3c682f76bfd435b326119dfcd27b7b
sim <s5.pentin@yandex.ru>
2023-10-09 11:38:06 +0300

    Исправлены неточности, удален неиспользуемый код

--- src/main/java/nicstore/controllers/AuthController.java
+++ src/main/java/nicstore/controllers/AuthController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.security.auth.message.AuthException;
 import javax.validation.Valid;
 import java.util.List;
 
