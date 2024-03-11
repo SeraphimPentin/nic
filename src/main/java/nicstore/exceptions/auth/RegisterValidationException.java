cd7082ed651e23cacf02fe8cb8e11c6349b23d25
SeraphimPentin <s5.pentin@yandex.ru>
2024-03-11 10:21:00 +0300

    fixed

--- src/main/java/nicstore/exceptions/auth/RegisterValidationException.java
+++
@@ -1,8 +0,0 @@
-package nicstore.exceptions.auth;
-
-public class RegisterValidationException extends RuntimeException{
-
-    public RegisterValidationException(String message){
-        super(message);
-    }
-}
